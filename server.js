42c04905bf104692ebf1e300bd4d78c49a313d62
Emmanuel Luciano <emma.luciano11@gmail.com>
2017-07-13 11:27:50 -0400

    Adds retries to request

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-var express = require("express");
-var url = require("url");
-var cors = require("cors");
-var qs = require("qs");
-var request = require("request");
-var queue = require("async/queue");
+const express = require("express");
+const url = require("url");
+const cors = require("cors");
+const qs = require("qs");
+const request = require("request");
+const queue = require("async/queue");
 
 module.exports = function() {
-  var app = express.Router();
+  const app = express.Router();
+  const MAX_RETRIES = 5;
 
   function validUrl(req, res, next) {
     req.query = req.query || qs.parse(url.parse(req.url).query);
@@ -24,22 +25,36 @@ module.exports = function() {
 
   app.use(cors());
   app.get("/", validUrl, function(req, res, next) {
+    let retryCount = 0;
+    let success = false;
+
     if (typeof req.query.callback === "string") {
-      queue(function() {
-        request({ url: req.query.url, encoding: "binary" }, function(
-          error,
-          response,
-          body
-        ) {
-          if (error) {
-            return next(error);
-          }
-          res.jsonp({
-            content: new Buffer(body, "binary").toString("base64"),
-            type: response.headers["content-type"]
+      do {
+        queue(function() {
+          request({ url: req.query.url, encoding: "binary" }, function(
+            error,
+            response,
+            body
+          ) {
+            if (error) {
+              retryCount += 1;
+            } else {
+              success = true;
+
+              res.jsonp({
+                content: new Buffer(body, "binary").toString("base64"),
+                type: response.headers["content-type"]
+              });
+            }
           });
-        });
-      }, 2);
+        }, 2);
+      } while (retryCount < MAX_RETRIES && !success);
+
+      // When retry count has been completed and we don't have success fallback to 500
+      // TODO: Make the fallback to a default image.
+      if (!success) {
+        return next(error);
+      }
     } else {
       req.pipe(request(req.query.url).on("error", next)).pipe(res);
     }
